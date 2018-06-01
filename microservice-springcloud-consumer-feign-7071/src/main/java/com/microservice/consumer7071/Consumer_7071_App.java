4a172e860dba8686dd7bb027aefdb0dba6e89664
HAHACJ <1148269987@qq.com>
2018-06-01 17:07:03 +0800

    增加feignpei服务端配置

--- microservice-springcloud-consumer-feign-7071/src/main/java/com/microservice/consumer7071/Consumer_7071_App.java
+++ microservice-springcloud-consumer-feign-7071/src/main/java/com/microservice/consumer7071/Consumer_7071_App.java
@@ -4,9 +4,11 @@ package com.microservice.consumer7071;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class Consumer_7071_App {
 
     public static void main(String[] args) {
