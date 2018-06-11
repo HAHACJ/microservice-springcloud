211df1dd330de30c426e1eaef89ad2e1b0e074dd
HAHACJ <1148269987@qq.com>
2018-06-11 16:41:37 +0800

    增加springconfig

--- microservice-springcloud-configclient-4040/src/main/java/com/microservice/ConfigClient_4040.java
+++ microservice-springcloud-configclient-4040/src/main/java/com/microservice/ConfigClient_4040.java
@@ -3,13 +3,11 @@ package com.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigService_5050 {
+public class ConfigClient_4040 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigService_5050.class, args);
+        SpringApplication.run(ConfigClient_4040.class, args);
     }
 }
